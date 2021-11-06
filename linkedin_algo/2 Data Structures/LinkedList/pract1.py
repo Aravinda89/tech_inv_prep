57a741c6d067cc10a27d2f1fafa1a7ad5274f95b
Aravinda89 <1513223865@qq.com>
2021-11-06 18:34:14 +0800

    Update pract1.py

--- linkedin_algo/2 Data Structures/LinkedList/pract1.py
+++ linkedin_algo/2 Data Structures/LinkedList/pract1.py
@@ -1,4 +1,3 @@
-
 # Node
 class Node(object):
     def __init__(self, val):
@@ -18,14 +17,16 @@ class Node(object):
         self.next = next
 
 class LinkedList(object):
-    def __init__(self, head):
+    def __init__(self, head=None):
         self.head = head
         self.count = 0
 
     def get_count(self):
         return self.count
 
-    def insert_item(self):
+    def dump_list(self):
+        tempnode = self.head
+        while(tempnode != None):
+            print(tempnode.get_val)
+            tempnode = tempnode.get_next()
         
-
-
